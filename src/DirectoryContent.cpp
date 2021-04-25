fa8d68fca1265182e8c81c0b005f3fe3bafc0517
DOCtorActoAntohich <fairy666death@mail.ru>
2021-04-25 12:02:21 +0300

    Implement recursive directory search

---
+++ src/DirectoryContent.cpp
@@ -0,0 +1,47 @@
+#include "DirectoryContent.h"
+
+#include <queue>
+
+DirectoryContent::DirectoryContent()
+{ }
+
+
+using path_t = std::filesystem::path;
+
+
+std::vector<path_t> DirectoryContent::getAll(const path_t& source) {
+    std::vector<path_t> contents;
+    for (const auto& entry : std::filesystem::directory_iterator(source)) {
+        contents.push_back(entry);
+    }
+    return contents;
+}
+
+
+std::vector<path_t> DirectoryContent::getAllRecursively(const path_t& source) {
+    std::vector<path_t> contents;
+
+    std::queue<path_t> dirsToCheck;
+    dirsToCheck.push(source);
+
+    while (!dirsToCheck.empty()) {
+        auto dir = dirsToCheck.front();
+        dirsToCheck.pop();
+
+        auto localContents = DirectoryContent::getAll(dir);
+        if (localContents.size() == 0) {
+            contents.push_back(dir);
+            continue;
+        }
+        for (auto& path : localContents) {
+            if (std::filesystem::is_directory(path)) {
+                dirsToCheck.push(path);
+            }
+            else {
+                contents.push_back(path);
+            }
+        }
+    }
+
+    return contents;
+}
---
+++ src/DirectoryContent.h
@@ -0,0 +1,17 @@
+#ifndef DIRECTORY_CONTENT_FINDER_H_INCLUDED
+#define DIRECTORY_CONTENT_FINDER_H_INCLUDED
+
+#include <vector>
+#include <filesystem>
+
+class DirectoryContent {
+public:
+    using path_t = std::filesystem::path;
+
+    static std::vector<path_t> getAll(const path_t& source);
+    static std::vector<path_t> getAllRecursively(const path_t& source);
+private:
+    DirectoryContent();
+};
+
+#endif // !DIRECTORY_CONTENT_FINDER_H_INCLUDED
